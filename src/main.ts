39c43659df61260cf72307bdf447aafbf82f731a
theblaze98 <dionijvargas@gmail.com>
2024-02-08 21:55:47 -0400

    Agregado Swagger para la Doc

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from "@nestjs/core";
+import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
 import { AppModule } from "./app.module";
 
 async function bootstrap() {
@@ -10,6 +11,17 @@ async function bootstrap() {
     origin: "*",
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
   });
+
+  const config = new DocumentBuilder()
+    .setTitle("Discord Api")
+    .setDescription("Discord clone API ")
+    .setVersion("1.0")
+    .addTag("discord")
+    .build();
+
+  const doc = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup("/doc", app, doc);
 
   await app.listen(Number(process.env.PORT) || 4000);
   console.log(`Server running on: ${await app.getUrl()}`);
